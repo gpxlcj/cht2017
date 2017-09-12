84647aee420baa13e336ef4246d7b0ea2c4f7185
gpxlcj <gpxlcj@gmail.com>
2017-09-12 20:08:25 +0800

    update external main

--- External/main.py
+++ External/main.py
@@ -25,18 +25,34 @@ def bus_main():
     # bus stop information
 
 
-def hsr_main():
-    pass
+def hsr_main(date):
+    try:
+        request_date = date[:4] + '-' + date[4:6] + '-' + date[6:]
+    except:
+        print('date format error')
+    hsr_station = External.HSR_station()
+    hsr_travel_time = External.HSR_travel_time(request_date)
+
+    hsr_travel_time_filename = 'hsr_travel_time_' + date + ".pickle"
+
+    pickle.dump(hsr_station, open("./external_data/hsr/hsr_station.pickle", "wb"))
+    pickle.dump(hsr_travel_time, open("./external_data/hsr/" + hsr_travel_time_filename, "wb"))
+
+    print "Data has been stored into 'external_data/hsr/hsr_station.pickle'"
+    print "Data has been stored into 'external_data/hsr/" + hsr_travel_time_filename + "'"
 
 
 def train_main():
-    pass
+    train_station = External.rail_station()
+    pickle.dump(train_station, open("./external_data/train/train_station.pickle", "wb"))
+    print "Data has been stored into 'external_data/hsr/train_station.pickle'"
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", type=str, help='(mrt/bus/hsr/train)')
+    parser.add_argument("-d", "--datetime", type=str, default='20170801', help="update datetime")
     args = parser.parse_args()
     print "Updating external data of mode: ", args.mode
     print "Start processing..."
